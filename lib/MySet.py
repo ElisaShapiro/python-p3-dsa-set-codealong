4f3a7210bf7b7ff79ac785abe49d688d44ebef38
Elisa Shapiro <elisashapiro@gmail.com>
2023-10-15 11:29:48 -0400

    tests passed

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,22 @@
 class MySet:
-
-    pass
+    def __init__(self, enumerable = []):
+        self.dictionary = {}
+        for value in enumerable:
+            self.dictionary[value] = True
+    def has(self, value):
+        return value in self.dictionary
+    def add(self, value):
+        self.dictionary[value] = True
+        return self
+    def delete(self, value):
+        self.dictionary.pop(value, None)
+        return self
+    def size(self):
+        return len(self.dictionary)
+    def clear(self):
+        self.dictionary.clear()
+    def __str__(self):
+        list_of_items = []
+        for key, value in self.dictionary.items():
+            list_of_items.append(str(key))
+        return f'MySet: {{{",".join(list_of_items)}}}'
